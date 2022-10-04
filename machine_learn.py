c428236e6d6e9ec8194e536a6867348258c88144
Neelima-maker <74758622+Neelima-maker@users.noreply.github.com>
2022-10-04 19:01:12 +0530

    Add files via upload

---
+++ machine_learn.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import tensorflow as tf
+
+model = tf.keras.models.load_model("keras_model.h5")
+
+
+vid = cv2.VideoCapture(0)
+
+while True:
+    check , image = vid.read()
+# resizing
+    img = cv2.resize(image ,(224,224))
+
+# Expansion of image
+
+    test_img = np.array(img,dtype = np.float32)
+
+    test_img = np.expand_dims(test_img, axis = 0)
+# Normalization
+    normalized_image  = test_img/255.0
+
+    prediction = model.predict(normalized_image)
+    print("Predicted :  ",  prediction)
+
+
+    cv2.imshow("result",image)
+
+    if cv2.waitKey(1)==32:
+        print("closing..")
+        break
+
+vid.release()
+
+
+    
